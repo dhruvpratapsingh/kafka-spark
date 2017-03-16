239aa72a0c9cdd740b7791a6616d90ef4532d37a
dhruv.singh <dhruv.singh@thomsonreuters.com>
2017-03-16 18:00:54 -0500

    Full working example

--- src/main/java/com/dhruv/SimpleStringProducer.java
+++ src/main/java/com/dhruv/SimpleStringProducer.java
@@ -1,12 +1,25 @@
 package com.dhruv;
 
+import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
 import org.apache.log4j.BasicConfigurator;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-public class SimpleStringProducer {
+public class SimpleStringProducer extends Thread {
+
+    private static final String fileName = "dataset/apache-access-logs.txt";
+    private static final String topicName = "mytopic";
+    private static final Boolean isAsync = true;
 
     public static void main(String[] args) {
         BasicConfigurator.configure();
@@ -16,17 +29,101 @@ public class SimpleStringProducer {
         props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
 
         KafkaProducer<String, String> producer = new KafkaProducer<>(props);
-        for (int i = 0; i < 100; i++) {
-            ProducerRecord<String, String> record = new ProducerRecord<>("mytopic", "value-" + i);
-            producer.send(record);
 
+        int lineCount = 0;
+        FileInputStream fis;
+        BufferedReader br = null;
+
+        try {
+            fis = new FileInputStream(fileName);
+            br = new BufferedReader(new InputStreamReader(fis));
+
+            String line;
+            String IPADDRESS_PATTERN =
+                "(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)";
+
+            Pattern pattern = Pattern.compile(IPADDRESS_PATTERN);
+            while ((line = br.readLine()) != null) {
+                String IP = getIPAddress(line, pattern);
+                lineCount++;
+                sendMessage(lineCount+"", IP, producer);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally{
             try {
-                Thread.sleep(250);
-            } catch (InterruptedException ie) {
-                System.out.print(ie);
+                br.close();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
         }
 
         producer.close();
+    }
+
+    private static String getIPAddress(String line, Pattern pattern) {
+        Matcher matcher = pattern.matcher(line);
+
+        if (matcher.find()) {
+            return matcher.group();
+        } else{
+            return "0.0.0.0";
+        }
+    }
+
+    private static void sendMessage(String key, String value, KafkaProducer<String, String> producer) {
+        long startTime = System.currentTimeMillis();
+        if (isAsync) {
+            producer.send(
+                new ProducerRecord<>(topicName, value),
+                (Callback) new DemoCallBack(startTime, key, value));
+        } else {
+            try {
+                producer.send(new ProducerRecord<>(topicName, key, value)).get();
+                System.out.println("Sent message: (" + key + ", " + value + ")");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+class DemoCallBack implements Callback {
+
+    private long startTime;
+    private String key;
+    private String message;
+
+    DemoCallBack(long startTime, String key, String message) {
+        this.startTime = startTime;
+        this.key = key;
+        this.message = message;
+    }
+
+    /**
+     * A callback method the user can implement to provide asynchronous handling
+     * of request completion. This method will be called when the record sent to
+     * the server has been acknowledged. Exactly one of the arguments will be
+     * non-null.
+     *
+     * @param metadata
+     *            The metadata for the record that was sent (i.e. the partition
+     *            and offset). Null if an error occurred.
+     * @param exception
+     *            The exception thrown during processing of this record. Null if
+     *            no error occurred.
+     */
+    public void onCompletion(RecordMetadata metadata, Exception exception) {
+        long elapsedTime = System.currentTimeMillis() - startTime;
+        if (metadata != null) {
+            System.out.println("message(" + key + ", " + message
+                + ") sent to partition(" + metadata.partition() + "), "
+                + "offset(" + metadata.offset() + ") in " + elapsedTime
+                + " ms");
+        } else {
+            exception.printStackTrace();
+        }
     }
 }
--- src/main/java/com/dhruv/SparkStringConsumer.java
+++ src/main/java/com/dhruv/SparkStringConsumer.java
@@ -1,22 +1,28 @@
 package com.dhruv;
 
+import kafka.serializer.StringDecoder;
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.PairFunction;
 import org.apache.spark.streaming.Duration;
+import org.apache.spark.streaming.api.java.JavaDStream;
+import org.apache.spark.streaming.api.java.JavaPairDStream;
 import org.apache.spark.streaming.api.java.JavaPairInputDStream;
 import org.apache.spark.streaming.api.java.JavaStreamingContext;
 import org.apache.spark.streaming.kafka.KafkaUtils;
+import scala.Tuple2;
+import net.jpountz.util.SafeUtils;
 
-import kafka.serializer.StringDecoder;
-
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
+import java.io.*;
+import java.util.*;
+import java.util.regex.Pattern;
 
 public class SparkStringConsumer {
 
-    public static void main(String[] args) {
+    private static final Pattern SPACE = Pattern.compile(" ");
+
+    public static void main(String[] args) throws Exception {
 
         SparkConf conf = new SparkConf()
             .setAppName("kafka-sandbox")
@@ -31,18 +37,19 @@ public class SparkStringConsumer {
         JavaPairInputDStream<String, String> directKafkaStream = KafkaUtils.createDirectStream(ssc,
             String.class, String.class, StringDecoder.class, StringDecoder.class, kafkaParams, topics);
 
-        directKafkaStream.foreachRDD(rdd -> {
-            System.out.println("--- New RDD with " + rdd.partitions().size()
-                + " partitions and " + rdd.count() + " records");
-            rdd.foreach(record -> System.out.println(record._2));
+        JavaDStream<String> lines = directKafkaStream.map(Tuple2::_2);
+        JavaDStream<String> words = lines.flatMap(x -> Arrays.asList(SPACE.split(x)).iterator());
+        JavaPairDStream<String, Integer> wordCounts = words.mapToPair(s -> new Tuple2<>(s, 1))
+            .reduceByKey((i1, i2) -> i1 + i2);
+
+        wordCounts.foreachRDD(rdd ->{
+            if(!rdd.isEmpty()){
+                rdd.coalesce(1).saveAsTextFile("dataset/apache-access-logs1.txt");
+            }
         });
 
         ssc.start();
 
-        try {
-            ssc.awaitTermination();
-        } catch (InterruptedException ie) {
-            System.out.print(ie);
-        }
+        ssc.awaitTermination();
     }
 }
